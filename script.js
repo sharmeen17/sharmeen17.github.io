1d21c742ac8a688946b56fcfae45fbb338d4d080
sharmeen17 <sharmeengardi@gmail.com>
2024-10-20 14:13:57 +0530

    Personal Portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function scrollCardsLeft(){
+    var wrapLeft = document.getElementById("wrapper");
+    wrapper.scrollLeft += 400;
+    console.log(wrapLeft);
+}
+
+function scrollCardsRight(){
+    var wrapLeft = document.getElementById("wrapper");
+    wrapper.scrollLeft -= 400;
+    console.log(wrapLeft);
+}
+
